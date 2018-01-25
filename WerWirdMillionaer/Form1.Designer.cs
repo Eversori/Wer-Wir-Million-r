806b473d0ec286133aabd206ab106f96e4355db9
Eversori <pauliniklas@dv-schulen.de>
2018-01-25 15:42:50 +0100

    gvfgdfg

--- WerWirdMillionaer/Form1.Designer.cs
+++ WerWirdMillionaer/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.normalPlay = new System.Windows.Forms.Button();
+            this.riskButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // normalPlay
+            // 
+            this.normalPlay.Location = new System.Drawing.Point(1002, 29);
+            this.normalPlay.Name = "normalPlay";
+            this.normalPlay.Size = new System.Drawing.Size(166, 78);
+            this.normalPlay.TabIndex = 0;
+            this.normalPlay.Text = "Normal";
+            this.normalPlay.UseVisualStyleBackColor = true;
+            // 
+            // riskButton
+            // 
+            this.riskButton.Location = new System.Drawing.Point(1002, 141);
+            this.riskButton.Name = "riskButton";
+            this.riskButton.Size = new System.Drawing.Size(166, 78);
+            this.riskButton.TabIndex = 1;
+            this.riskButton.Text = "Risiko";
+            this.riskButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(298, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.riskButton);
+            this.Controls.Add(this.normalPlay);
+            this.Name = "Form1";
+            this.Text = "Wer wird Millionär";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button normalPlay;
+        private System.Windows.Forms.Button riskButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
