64748731376d517d667a5bde485f26a0ca4f3231
JaekelJulian <JaekelJulian@dv-schulen.de>
2018-01-29 12:43:57 +0100

    Added Joker.cs Antwort.cs Frage.cs

---
+++ WerWirdMillionaer/Joker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WerWirdMillionaer
+{
+    class Joker
+    {
+        Boolean benutzt;
+        String name;
+        Boolean viable;
+
+    }
+}
