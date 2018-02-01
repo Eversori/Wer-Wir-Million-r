d645a8f948c48ee4421b70ac84a59799b11b84f1
Eversori <pauliniklas@dv-schulen.de>
2018-02-01 13:57:20 +0100

    Spielfenster Grundlage fertig

--- WerWirdMillionaer/Spielfenster.Designer.cs
+++ WerWirdMillionaer/Spielfenster.Designer.cs
@@ -28,7 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonA = new System.Windows.Forms.Button();
+            this.buttonC = new System.Windows.Forms.Button();
+            this.buttonB = new System.Windows.Forms.Button();
+            this.buttonD = new System.Windows.Forms.Button();
+            this.pictureBoxFrage = new System.Windows.Forms.PictureBox();
+            this.labelFrage = new System.Windows.Forms.Label();
+            this.button5050 = new System.Windows.Forms.Button();
+            this.buttonTeleJoker = new System.Windows.Forms.Button();
+            this.buttonPubJoker = new System.Windows.Forms.Button();
+            this.buttonZusatzJoker = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFrage)).BeginInit();
             this.SuspendLayout();
+            // 
+            // buttonA
+            // 
+            this.buttonA.Location = new System.Drawing.Point(130, 505);
+            this.buttonA.Name = "buttonA";
+            this.buttonA.Size = new System.Drawing.Size(325, 23);
+            this.buttonA.TabIndex = 0;
+            this.buttonA.Text = "Antwort A";
+            this.buttonA.UseVisualStyleBackColor = true;
+            // 
+            // buttonC
+            // 
+            this.buttonC.Location = new System.Drawing.Point(130, 568);
+            this.buttonC.Name = "buttonC";
+            this.buttonC.Size = new System.Drawing.Size(325, 23);
+            this.buttonC.TabIndex = 1;
+            this.buttonC.Text = "Antwort C";
+            this.buttonC.UseVisualStyleBackColor = true;
+            // 
+            // buttonB
+            // 
+            this.buttonB.Location = new System.Drawing.Point(803, 505);
+            this.buttonB.Name = "buttonB";
+            this.buttonB.Size = new System.Drawing.Size(325, 23);
+            this.buttonB.TabIndex = 2;
+            this.buttonB.Text = "Antwort B";
+            this.buttonB.UseVisualStyleBackColor = true;
+            // 
+            // buttonD
+            // 
+            this.buttonD.Location = new System.Drawing.Point(803, 568);
+            this.buttonD.Name = "buttonD";
+            this.buttonD.Size = new System.Drawing.Size(325, 23);
+            this.buttonD.TabIndex = 3;
+            this.buttonD.Text = "Antwort D";
+            this.buttonD.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxFrage
+            // 
+            this.pictureBoxFrage.Location = new System.Drawing.Point(486, 368);
+            this.pictureBoxFrage.Name = "pictureBoxFrage";
+            this.pictureBoxFrage.Size = new System.Drawing.Size(312, 50);
+            this.pictureBoxFrage.TabIndex = 4;
+            this.pictureBoxFrage.TabStop = false;
+            // 
+            // labelFrage
+            // 
+            this.labelFrage.AutoSize = true;
+            this.labelFrage.Location = new System.Drawing.Point(620, 389);
+            this.labelFrage.Name = "labelFrage";
+            this.labelFrage.Size = new System.Drawing.Size(34, 13);
+            this.labelFrage.TabIndex = 5;
+            this.labelFrage.Text = "Frage";
+            // 
+            // button5050
+            // 
+            this.button5050.Location = new System.Drawing.Point(12, 21);
+            this.button5050.Name = "button5050";
+            this.button5050.Size = new System.Drawing.Size(116, 23);
+            this.button5050.TabIndex = 6;
+            this.button5050.Text = "50 : 50";
+            this.button5050.UseVisualStyleBackColor = true;
+            // 
+            // buttonTeleJoker
+            // 
+            this.buttonTeleJoker.Location = new System.Drawing.Point(12, 50);
+            this.buttonTeleJoker.Name = "buttonTeleJoker";
+            this.buttonTeleJoker.Size = new System.Drawing.Size(116, 23);
+            this.buttonTeleJoker.TabIndex = 7;
+            this.buttonTeleJoker.Text = "Telefonjoker";
+            this.buttonTeleJoker.UseVisualStyleBackColor = true;
+            // 
+            // buttonPubJoker
+            // 
+            this.buttonPubJoker.Location = new System.Drawing.Point(12, 79);
+            this.buttonPubJoker.Name = "buttonPubJoker";
+            this.buttonPubJoker.Size = new System.Drawing.Size(116, 23);
+            this.buttonPubJoker.TabIndex = 8;
+            this.buttonPubJoker.Text = "Publikumsjoker";
+            this.buttonPubJoker.UseVisualStyleBackColor = true;
+            // 
+            // buttonZusatzJoker
+            // 
+            this.buttonZusatzJoker.Location = new System.Drawing.Point(12, 108);
+            this.buttonZusatzJoker.Name = "buttonZusatzJoker";
+            this.buttonZusatzJoker.Size = new System.Drawing.Size(116, 23);
+            this.buttonZusatzJoker.TabIndex = 9;
+            this.buttonZusatzJoker.Text = "Zusatzjoker";
+            this.buttonZusatzJoker.UseVisualStyleBackColor = true;
             // 
             // Spielfenster
             // 
@@ -37,12 +137,35 @@
             this.BackgroundImage = global::WerWirdMillionaer.Properties.Resources._91OXosjxZdL;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1284, 641);
+            this.Controls.Add(this.buttonZusatzJoker);
+            this.Controls.Add(this.buttonPubJoker);
+            this.Controls.Add(this.buttonTeleJoker);
+            this.Controls.Add(this.button5050);
+            this.Controls.Add(this.labelFrage);
+            this.Controls.Add(this.pictureBoxFrage);
+            this.Controls.Add(this.buttonD);
+            this.Controls.Add(this.buttonB);
+            this.Controls.Add(this.buttonC);
+            this.Controls.Add(this.buttonA);
             this.Name = "Spielfenster";
             this.Text = "Spielfenster";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFrage)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonA;
+        private System.Windows.Forms.Button buttonC;
+        private System.Windows.Forms.Button buttonB;
+        private System.Windows.Forms.Button buttonD;
+        private System.Windows.Forms.PictureBox pictureBoxFrage;
+        private System.Windows.Forms.Label labelFrage;
+        private System.Windows.Forms.Button button5050;
+        private System.Windows.Forms.Button buttonTeleJoker;
+        private System.Windows.Forms.Button buttonPubJoker;
+        private System.Windows.Forms.Button buttonZusatzJoker;
     }
 }
